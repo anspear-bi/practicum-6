fe11a848729159321ca04defe25480cb52c895d4
anspear-bi <a.starikova3@g.nsu.ru>
2025-03-24 10:53:31 +0700

    Update 6.5.py

--- 6.5.py
+++ 6.5.py
@@ -3,28 +3,20 @@
 6.5
 """
 
+def good(begin, end):
+    begin_x = ord(begin[0]) - ord('a')
+    begin_y = int(begin[1]) - 1
+    end_x = ord(end[0]) - ord('a')
+    end_y = int(end[1]) - 1
+
+    dx = abs(begin_x - end_x)
+    dy = abs(begin_y - end_y)
+
+    return (dx == 2 and dy == 1) or (dx == 1 and dy == 2)
+
+move = input("Введите ход: ")
 begin, end = map(input('Введите ход (через дефис): ').split('-'))
-
-def good_waste(start, end):
-    '''
-    
-
-    Parameters
-    ----------
-    start : TYPE
-        DESCRIPTION.
-    end : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    None.
-
-    '''
-
-
-if good_waste(begin, end):
+if good(begin, end):
     print("Верно")
 else:
     print("Ошибка")
-
